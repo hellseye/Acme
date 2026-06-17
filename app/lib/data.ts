252d7a6d6c3b31080cfaa0a00ec7184ab7f2f5cc
hellseye <mayankdhanuka899@gmail.com>
2026-06-17 21:41:01 +0530

    make skeleton loading

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -14,7 +14,12 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 export async function fetchRevenue() {
   try {
 
+    // console.log('Fetching revenue data...');
+    // await new Promise((resolve) => setTimeout(resolve, 30000));
+
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
+
+    // console.log('Data fetch completed after 3 seconds.');
 
     return data;
   } catch (error) {
